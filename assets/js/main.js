8560e6df6f4753af869f9134e66b9f88715b184d
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-05-20 04:22:03 -0300

    Update products

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-import { products } from "./products.js";
-
 const navItem = document.querySelectorAll('.navbar ul li a');
 const productsList = document.querySelector('.products-list');
 
@@ -8,29 +6,44 @@ function activateSection() {
 	navItem.forEach(item => item.classList.remove('active'));
 	this.classList.add('active');
 
-	products.forEach(product => {
-		if (product.type.toLowerCase() === this.id) {
-			productsList.innerHTML += `
-			<section class="product-item">
-				<h1>${product.name}</h1>
-				<img src=${product.img} alt=${product.name} />
-				<a href="https://shopee.com.br/sublimeartespersonalizadas">Informações</a>
-			</section>
-			`;
-		}
-	})
+	fetch("/products.json")
+		.then(res => res.json())
+		.then(data => {
+			for (const key in data) {
+				if (key === this.id) {
+					const products = data[key];
+					products.forEach(product => {
+						productsList.innerHTML += `
+							<section class="product-item">
+								<h1>${product.nome}</h1>
+								<img src=${product.img} alt=${product.name} loading="lazy" />
+								<a href="https://shopee.com.br/sublimeartespersonalizadas">Informações</a>
+							</section>
+							`;
+					});
+				}
+			};
+		});
 }
 
-products.forEach(product => {
-	productsList.innerHTML += `
-	<section class="product-item">
-		<h1>${product.name}</h1>
-		<img src=${product.img} alt=${product.name} />
-		<a href="https://shopee.com.br/sublimeartespersonalizadas">Informações</a>
-	</section>
-	`;
-})
+navItem.forEach(item => item.addEventListener('click', activateSection));
 
-navItem.forEach(item => {
-	item.addEventListener('click', activateSection);
-})
+// Load all products
+fetch("/products.json")
+	.then(res => res.json())
+	.then(data => {
+		for (const key in data) {
+			const products = data[key];
+
+			products.forEach(product => {
+				productsList.innerHTML += `
+					<section class="product-item">
+						<h1>${product.nome}</h1>
+						<img src=${product.img} alt=${product.name} />
+						<a href="https://shopee.com.br/sublimeartespersonalizadas">Informações</a>
+					</section>
+					`;
+			});
+		};
+	});
+
